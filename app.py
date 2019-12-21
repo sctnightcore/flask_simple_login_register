f78cd8264165ca24a437accaf2d4b8e43c70ea88
sctnightcore <nightcoremax@gmail.com>
2019-12-21 15:42:42 +0700

    update Code !

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from flask import Flask
-from router import router_blueprint
-from views.api import api_blueprint
 
 def create_app():
 	app = Flask(__name__)
 	app.config.from_object("settings.Config")
-	#router
-	app.register_blueprint(router_blueprint, url_prefix="/")
-	#api
-	app.register_blueprint(api_blueprint, url_prefix="/api")
-	return app
+
+	with app.app_context():
+		from router import router_blueprint
+		from views.api import api_blueprint
+		#router
+		app.register_blueprint(router_blueprint, url_prefix="/")
+		#api
+		app.register_blueprint(api_blueprint, url_prefix="/api")
+		return app
 
 
 if __name__ == "__main__":
